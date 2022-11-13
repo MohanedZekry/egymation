1db6b26977e576822c45142bd785115ecedb22c4
MohanedZekry <mohanadzekry@gmail.com>
2022-11-14 00:13:22 +0200

    add navigation 🧊 cubit 🧊 & cache 💾

---
+++ lib/infrastructure/cache/cache.dart
@@ -0,0 +1,19 @@
+class CacheClient {
+  CacheClient() : _cache = <String, Object>{};
+
+  final Map<String, Object> _cache;
+
+  void write<T extends Object>({required String key, required T value}) {
+    _cache[key] = value;
+  }
+
+  void remove<T extends Object>({required String key}) {
+    _cache.remove(key);
+  }
+
+  T? read<T extends Object>({required String key}) {
+    final value = _cache[key];
+    if (value is T) return value;
+    return null;
+  }
+}
